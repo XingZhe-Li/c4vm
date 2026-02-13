d4467dcebda638d348ec706afeec31878f1daf1c
XingZhe-Li <xingzhe_li@qq.com>
2026-02-13 15:39:36 +0800

    compiler define macros

--- compiler.py
+++ compiler.py
@@ -50,18 +50,70 @@ def c4vm_c_preprocess_include(src: str, src_path_root: str) -> str:
     return '\n'.join(buffer)
 
 def c4vm_c_preprocess_defines(src: str):
-    # TODO
-    ...
+    buffer = []
+    state_stack = []
+    define_table = {}
+
+    for ln in src.split('\n'):
+        raw_ln = ln
+        ln_strip = ln.strip()
+        
+        if ln_strip.startswith("#"):
+            lnsplit = ln_strip.split()
+            cmd = lnsplit[0]
+            
+            if cmd == "#define":
+                if len(lnsplit) == 2:
+                    define_table[lnsplit[1]] = ""
+                elif len(lnsplit) >= 3:
+                    define_table[lnsplit[1]] = " ".join(lnsplit[2:])
+                continue
+                
+            elif cmd == "#undef":
+                define_table.pop(lnsplit[1], None)
+                continue
+                
+            elif cmd == "#ifdef":
+                active = (lnsplit[1] in define_table)
+                state_stack.append(active)
+                continue
+
+            elif cmd == "#ifndef":
+                active = (lnsplit[1] not in define_table)
+                state_stack.append(active)
+                continue
+
+            elif cmd == "#else":
+                if state_stack:
+                    state_stack[-1] = not state_stack[-1]
+                continue
+
+            elif cmd == "#endif":
+                if state_stack:
+                    state_stack.pop()
+                continue
+
+        if all(state_stack):
+            processed_ln = raw_ln
+            if define_table:
+                for name in sorted(define_table.keys(), key=len, reverse=True):
+                    pattern = r'\b' + re.escape(name) + r'\b'
+                    processed_ln = re.sub(pattern, str(define_table[name]), processed_ln)
+            
+            buffer.append(processed_ln)
+
+    return "\n".join(buffer)
 
 def c4vm_c_preprocess(src: str,src_path_root: str):
     src = c4vm_c_preprocess_remove_comment(src)
     src = c4vm_c_preprocess_include(src,src_path_root)
     src = c4vm_c_preprocess_defines(src)
+    return src
 
 def c4vm_c_compile(src_entry):
     if not os.path.isfile(src_entry):
         print("[ERRO] c4vm_c_compile : {0} does not exist".format(src_entry))
-        sys.exit()
+        sys.exit(1)
     src_path_root = os.path.dirname(src_entry)
     try:
         with open(src_entry,'r',encoding='utf-8') as f:
@@ -71,7 +123,7 @@ def c4vm_c_compile(src_entry):
         print("[ERRO] c4vm_c_compile : failed when opening {0}".format(src_entry))
         sys.exit(1)
     src = c4vm_c_preprocess(src,src_path_root)
-    ... # TODO
+    print(src)
 
 def c4vm_pack(program_bytes,space: int):
     return space.to_bytes(8,'little') + program_bytes
