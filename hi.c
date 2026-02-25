c966d4c004573535846adda7f8b5cbcfe1ed02eb
XingZhe-Li <xingzhe_li@qq.com>
2026-02-25 20:32:10 +0800

    end demo!

--- hi.c
+++ hi.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-struct Person {
-    long long* base;
-};
-
 int main(int argc,char** argv) {
-    int x[1] = {321123};
-    struct Person bob = {
-        .base = x
-    };
-    printf(
-        "%lld %lld %lld",
-        (char*)bob.base,
-        bob.base[0],
-        (char*)bob.base + bob.base[0]
-    );
+    printf("Hello World!!!");
 }
